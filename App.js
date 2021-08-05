22e6a9b8bf8bb2cd5c5638c36fb3ed7d99f2fe8e
Hristo <h.georgiew93@gmail.com>
2021-08-05 17:00:29 +0300

    Create Login page

--- App.js
+++ App.js
@@ -4,13 +4,28 @@ import { Button, TextInput } from 'react-native-paper';
 import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 
 import SignUpScreen from './components/Auth/SignUpScreen';
+import LoginScreen from './components/Auth/LoginScreen';
+import LoadingScreen from './components/Loading/LoadingScreen';
+import HomeScreen from './components/Home/HomeScreen';
 
 export default function App() {
   return (
-    <>
-      <SignUpScreen />
-    </>
+    <View style={styles.loading}>
+      {/* <SignUpScreen />*/}
+      {/*<LoginScreen />*/}
+      {/*<LoadingScreen />*/}
+      <HomeScreen />
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  loading:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
+
 
 
---
+++ components/Auth/LoginScreen.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <>
+      <KeyboardAvoidingView behavior="padding">
+        <StatusBar backgroundColor="gray" barStyle="light-content" />
+        <Text style={styles.title}>Welcome to</Text>
+        <Text style={styles.logo}>Nsys App</Text>
+        <View style={styles.line}></View>
+        <Text style={styles.newAcount}>Login</Text>
+
+    
+        <TextInput
+          style={styles.input}
+          label="Email"
+          mode="outlined"
+          theme={{ colors: { primary: "blue" } }}
+        />
+        <TextInput
+          style={styles.input}
+          label="Password"
+          mode="outlined"
+          theme={{ colors: { primary: "blue" } }}
+        />
+
+        <Button
+          style={styles.input}
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+           Влез
+        </Button>
+        <TouchableOpacity >
+          <Text style={styles.input}>Нямате регистрация ?</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  line: {
+    borderBottomColor: "blue",
+    borderBottomWidth: 4,
+    borderRadius: 10,
+    marginLeft: 20,
+    marginRight: 150,
+    marginTop: 4
+  },
+
+  logo: {
+    fontSize: 30,
+    marginLeft: 18,
+    color: "blue"
+  },
+
+  title: {
+    fontSize: 35,
+    marginLeft: 18,
+    marginTop: 50,
+    color: "gray"
+  },
+  newAcount: {
+    fontSize: 20,
+    marginLeft: 18,
+    marginTop: 20
+  },
+
+  input: {
+    marginLeft: 18,
+    marginRight: 18,
+    marginTop: 18,
+
+  }
+});
+
+export default LoginScreen;
